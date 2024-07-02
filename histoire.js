a3e68102f07b522e990303915487acb1ece03099
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2024-07-02 21:55:14 +0200

    Update histoire.js

--- histoire.js
+++ histoire.js
@@ -1,6 +1,6 @@
       document.getElementById("logo").addEventListener("click", function () {
         window.location.href = "index.html";
-      });
+      })
 
       document.getElementById('music').volume = 0.3;
       window.onload("click", function () {
