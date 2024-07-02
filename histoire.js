77151cc82216a1450dc7edce6f557b8346d3014a
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2024-07-02 21:48:41 +0200

    Create histoire.js

---
+++ histoire.js
@@ -0,0 +1,9 @@
+      document.getElementById("logo").addEventListener("click", function () {
+        window.location.href = "index.html";
+      });
+
+      document.getElementById('music').volume = 0.3;
+      window.onload("click", function () {
+        var audio = document.getElementById("music");
+        audio.play();
+      });
