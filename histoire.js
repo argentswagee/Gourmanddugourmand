1e627f0e40c151e1578c5dc9ac2ec33d8adecfba
Edouard Auchet <125192936+argentswagee@users.noreply.github.com>
2024-07-02 22:03:50 +0200

    Delete histoire.js

--- histoire.js
+++
@@ -1,6 +0,0 @@
-      document.getElementById("logo").addEventListener("click", function () {
-        window.location.href = "index.html";
-      })
-
-      document.getElementById('music').volume = 0.3;
-alert(bonjour);
